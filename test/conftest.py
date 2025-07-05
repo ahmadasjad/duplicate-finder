888b13c7e0f04c04525f7a429887ab51f98eb061
Ahmad Asjad <ahmadcimage@gmail.com>
2025-07-05 14:40:27 +0530

    no test for the time

--- test/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Pytest configuration file."""
-
-import os
-import sys
-
-# Add the app directory to Python path for imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
